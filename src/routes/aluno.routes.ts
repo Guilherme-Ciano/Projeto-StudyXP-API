e93b191c026ca10a4de3c32aa581701ffde240ce
Guilherme Ciano <gui_ciano31@yahoo.com>
2021-06-28 20:54:56 -0300

    add: Multer Configs

--- src/routes/aluno.routes.ts
+++ src/routes/aluno.routes.ts
@@ -1,10 +1,19 @@
 import { Router } from 'express'
 import AlunoController from './../database/controllers/Aluno.controller';
 
+const multer = require('multer');
+const multerConfig = require('../config/multer');
 const alunosRoutes = Router()
 const alunoController = new AlunoController()
 
 alunosRoutes.get('/index', alunoController.index)
+
 alunosRoutes.post('/create', alunoController.create)
+
+alunosRoutes.post('/img', multer(multerConfig).single('file'), (req, res) => {
+    console.log(req.file);
+    return res.json({status: "Success"})
+})
+
 
 export default alunosRoutes
--- src/routes/professor.routes.ts
+++ src/routes/professor.routes.ts
@@ -3,11 +3,18 @@ import ProfessorController from './../database/controllers/Professor.controller'
 import materiaRoutes from './materia.routes';
 import tarefaRoutes from './tarefa.routes';
 
+const multer = require('multer');
+const multerConfig = require('../config/multer');
 const professoresRoutes = Router()
 const professorController = new ProfessorController()
 
 professoresRoutes.get('/index', professorController.index)
 professoresRoutes.post('/create', professorController.create)
+
+professoresRoutes.post('/img', multer(multerConfig).single('file'), (req, res) => {
+    console.log(req.file);
+    return res.json({status: "Success"})
+})
 
 professoresRoutes.use('/tarefas', tarefaRoutes)
 professoresRoutes.use('/materias', materiaRoutes)

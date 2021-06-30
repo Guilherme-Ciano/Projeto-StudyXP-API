f4de432eebafe8532965cd4ee5334100d8589c24
Guilherme Ciano <gui_ciano31@yahoo.com>
2021-06-30 20:31:21 -0300

    add: hash de login

--- src/routes/session.routes.ts
+++ src/routes/session.routes.ts
@@ -2,30 +2,43 @@ import { response, Router } from 'express'
 import { getRepository } from 'typeorm';
 import Aluno from './../database/model/Aluno';
 import Professor from './../database/model/Professor';
+import crypto from 'crypto';
 
 const sessionLog = Router()
 
 sessionLog.post('/', async (req, res) => {
-    const { email, password } = req.body;
-
-    const alunoRepository = getRepository(Aluno);
+    // Token de Segurança
+    var current_date = (new Date()).valueOf().toString();
+    var random = Math.random().toString();
+    var hashCod = crypto.createHash('sha1').update(current_date + random).digest('hex');
     
-    let aluno = await alunoRepository.findOne({ email });
+    // Verificação
+    const { email, password } = req.body;
+    const alunoRepository = getRepository(Aluno);
+    let aluno = await alunoRepository.findOne({ email } && { password });
     
     if (!aluno) {
         const professorRepository = getRepository(Professor);
-        let professor = await professorRepository.findOne({ email });
+        let professor = await professorRepository.findOne({where: { email }} && {where: { password }});
+
+        let responseProf = {
+            data: professor,
+            hash: hashCod
+        }
 
         if (professor) {
-            return res.json(professor);
+            return res.json(responseProf);
         } else {
             return res.json({
                 msg: "ERRO!"
             })
         }
     }
-    
-    return res.json(aluno);
+    let responseAluno = {
+        data: aluno,
+        hash: hashCod
+    }
+    return res.json(responseAluno);
 })
 
 export default sessionLog;
